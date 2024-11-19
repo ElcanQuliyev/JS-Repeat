b16dacaef58c738474547f1fda0dedf81a6df1d1
elcanquliyevf <quliyevelcan112233@gmail.com>
2024-11-19 19:48:46 +0400

    Lesson-1 repeat

---
+++ 1/main.js
@@ -0,0 +1,31 @@
+// Ekrana bildirim cixardir
+
+// alert("Salam");
+
+// window.alert("Salam");
+
+// --------------------------------
+// --------------------------------
+
+// Web sehvede yazir
+// Ferqi html-de PRE taginden isdifade edirik ve her ikisini writeln yazsaq alt-alta dusur
+
+// document.write("Salam");
+
+// document.writeln("Salam");
+
+// --------------------------------
+// --------------------------------
+
+// console.log("bura console-du ve bura yazi yaz");
+
+// debugger; yazaraq yazdigimiz kodu dayandirib yeniden islede bilerik
+
+// --------------------------------
+// --------------------------------
+
+// Inner Html
+
+document.getElementById("salam-1").innerHTML = "Hello";
+document.getElementById("dunya-1").innerHTML = "World!";
+document.getElementById("bos").innerHTML = "JS-le icine metn yazdiq";
