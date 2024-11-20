ffbfa87a14a0bd097d5446d72efb7e84cc6d8f5f
elcanquliyevf <quliyevelcan112233@gmail.com>
2024-11-20 21:58:39 +0400

    Lesson-2 repeat

---
+++ 2/main.js
@@ -0,0 +1,69 @@
+// Data Types
+
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Array
+// Object
+
+// string
+// let x = "Elcan";
+// console.log(x);
+// console.log(typeof x);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// number
+// let x = 10;
+// console.log(x);
+// console.log(typeof x);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// bigint
+// let x = BigInt("123456789012345678901234567890");
+// console.log(x);
+// console.log(typeof x);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// boolean
+// let x = true;
+// console.log(x);
+// console.log(typeof x);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// undifined
+// let x ;
+// console.log(x);
+// console.log(typeof x);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// null  typeof-object verir
+// let x = null;
+// console.log(x);
+// console.log(typeof x);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// array
+// let cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars[2]);
+
+// ----------------------------------------
+// ----------------------------------------
+
+// object
+let person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+console.log(person.age);
